a0e991fcd8913c8cdd1226ee3bff45a3a40ecd2e
Mbunkur <alainmbunkur@gmail.com>
2023-06-24 06:00:28 +0200

    backend done

---
+++ config/app.config.js
@@ -0,0 +1,11 @@
+
+require('dotenv').config;
+
+const MONGO_DB_CONFIG = {
+    DB: "mongodb+srv://alainmbunkur:J7q4tqfuvisjKMNw@reactbackendportfolio.0s7bltd.mongodb.net/ReactBackendProject?retryWrites=true&w=majority"
+}
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
